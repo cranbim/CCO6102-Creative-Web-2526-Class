b8ae8252404d5b70b8145e73f06d36ec86e9a842
Dave Webb <dave.webb@cranbim.com>
2025-09-30 08:07:08 +0100

    Week1 final

---
+++ Week01/firstAPI/code.js
@@ -0,0 +1,43 @@
+const latlong = document.getElementById("location");
+
+const myPosition={
+    isActive:false,
+    lat:0,
+    long:0
+}
+
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error);
+  } else {
+    latlong.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function success(position) {
+    console.log(position)
+    myPosition.isActive=true
+    myPosition.lat=position.coords.latitude
+    myPosition.long=position.coords.longitude
+  latlong.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+function error() {
+  alert("Sorry, no position available.");
+}
+
+function getSunTimes(){
+    let urlBase="https://api.sunrise-sunset.org/json?"
+    let query=urlBase+"lat="+myPosition.lat+"&lng="+myPosition.long
+    fetch(query)
+        .then(jdata=>jdata.json())
+        .then(processSunTimes)
+}
+
+function processSunTimes(sunData){
+    .innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude
+}
+
