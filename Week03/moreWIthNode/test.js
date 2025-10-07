19e655ee0d31def0012547e547d5973eda9ed1ed
Dave Webb <dave.webb@cranbim.com>
2025-10-07 09:24:04 +0100

    Week 3 prep

---
+++ Week03/moreWIthNode/test.js
@@ -0,0 +1,12 @@
+// const luckyNumber=43
+// const secretNumber=97
+
+// function addSecret(yourNum){
+//     return yourNum+secretNumber
+// }
+
+const utils=require('./utils.js')
+
+console.log(utils.luckyNumber)
+console.log(utils.addSecret(1000))
+console.log(utils.secretNumber)
---
+++ Week03/moreWIthNode/utils.js
@@ -0,0 +1,11 @@
+const luckyNumber=43
+const secretNumber=97
+
+function addSecret(yourNum){
+    return yourNum+secretNumber
+}
+
+module.exports={
+    luckyNumber,
+    addSecret
+}
