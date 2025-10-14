0b9cb1663aa56e78d88a33d543540bb433b28a6e
Dave Webb <dave.webb@cranbim.com>
2025-10-14 08:02:05 +0100

    week4 prep

---
+++ Week04/WeatherAPI/code.js
@@ -0,0 +1,26 @@
+const apikey='4463216da4224c8387d144501241610'
+// const queryurl='http://api.weatherapi.com/v1/current.json?key=4463216da4224c8387d144501241610&q=Bristol&aqi=no'
+const City='Bath'
+const queryurl='http://api.weatherapi.com/v1/current.json'+
+                '?'+'key='+apikey+
+                '&'+'q='+City+
+                '&'+'aqi='+'no'
+
+                let counter=0;
+                let countInterval;
+                
+                
+function getData() {
+    countInterval=setInterval(()=>console.log(counter++/10),100);
+    fetch(queryurl)
+        .then(jdata=>jdata.json())
+        .then(data=>{
+            clearInterval(countInterval);
+            console.log(data)
+        })
+    .catch(error => console.log('Error while fetching:', error))
+}
+
+
+
+
